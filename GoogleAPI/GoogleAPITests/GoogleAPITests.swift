cefeb94a157156ba93179e0d4dd8bbb5dcfece8d
rodrigo-placido-apontador <rodrigo.placido@gmail.com>
2016-11-06 18:56:50 -0200

    Unit tests

--- GoogleAPI/GoogleAPITests/GoogleAPITests.swift
+++ GoogleAPI/GoogleAPITests/GoogleAPITests.swift
@@ -10,10 +10,21 @@ import XCTest
 @testable import GoogleAPI
 
 class GoogleAPITests: XCTestCase {
-    
+    var placesArray = NSMutableArray()
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        let masterDataUrl: NSURL = Bundle.main.url(forResource: "place", withExtension: "json")! as NSURL
+        
+        let jsonData: NSData = NSData(contentsOf: masterDataUrl as URL)!
+        do {
+            let jsonResult: NSDictionary = try JSONSerialization.jsonObject(with: jsonData as Data, options:  JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
+            let modelConverter: PlaceConverter = PlaceConverter()
+            placesArray = modelConverter.convertModel(json: jsonResult)
+        } catch {
+            print("json error: \(error)")
+        }
+
     }
     
     override func tearDown() {
@@ -21,16 +32,50 @@ class GoogleAPITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testPlacesCount() {
+        XCTAssertEqual(placesArray.count, 6)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testPlace() {
+        let place: Place = placesArray[0] as! Place
+        XCTAssertEqual(place.name, "The Bay Detail")
+        XCTAssertEqual(place.vicinity, "45 3rd St, San Francisco")
+        XCTAssertEqual(place.icon, "https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png")
+        XCTAssertEqual(place.openingHours?.openNow, "1")
+    }
+
+    
+    func testErrorCode() {
+        let errorHelper = ErrorHelper()
+        
+        var message: NSString = errorHelper.getErrorMessage(statusCode:-1009) as NSString
+        XCTAssertEqual(message, "No connection, check and try again")
+        
+        message = errorHelper.getErrorMessage(statusCode:403) as NSString
+        XCTAssertEqual(message, "Request Error")
+        
+        message = errorHelper.getErrorMessage(statusCode:503) as NSString
+        XCTAssertEqual(message, "Service unavailable")
+    }
+
+    func testOpenNowColor() {
+        let placeUtil = PlaceUtils()
+        
+        var color = placeUtil.getOpenNowColor(openNow: "1")
+        XCTAssertEqual(color, UIColor.green)
+        
+        color = placeUtil.getOpenNowColor(openNow: "0")
+        XCTAssertEqual(color, UIColor.red)
     }
     
+    func testOpenNowText() {
+        let placeUtil = PlaceUtils()
+        
+        var message = placeUtil.getOpenNowString(openNow: "1")
+        XCTAssertEqual(message, "Aberto")
+        
+        message = placeUtil.getOpenNowString(openNow: "0")
+        XCTAssertEqual(message, "Fechado")
+    }
+
 }
